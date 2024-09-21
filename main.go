20bb1e5262a445ade9d89b4dedbbd2dfdd4b2a0a
Ernest4ik <148816228+Ernest4ik@users.noreply.github.com>
2024-09-21 20:54:39 +0300

    Add Post request

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import(
   "github.com/gin-gonic/gin"
+  "net/http"
 )
 
-func main() {
+type book struct{
+  Title: string `json "title"`
+  Description: string `json "description"`
+  Price: int `json "price"`
+  Author: string `json author`
+}
 
+var books []book
+
+func postBook(c *gin.Context){
+    var curBook book
+    if err !=  c.BindJson(&curBook), err!=nil{
+      return
+    }
+    books = append(books, curBook)
+    c.IndentedJson(http.StatusCreated, curBook)
+    
+}
+
+func main() {
+    router := gin.Default()
+    router.POST("/book")
+    router.Run("localhost:8080")
 }
