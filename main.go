4c8671dec4222f919b9ac0d2f050643ff6a0bd52
somsom <you@example.com>
2024-09-21 20:57:33 +0300

    asdasd

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
 package main
 
-import(
-  "github.com/gin-gonic/gin"
-  "net/http"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-type book struct{
-  Title: string `json "title"`
-  Description: string `json "description"`
-  Price: int `json "price"`
-  Author: string `json author`
+type book struct {
+	Title       string `json "title"`
+	Description string `json "description"`
+	Price       int    `json "price"`
+	Author      string `json author`
 }
 
 var books []book
 
-func postBook(c *gin.Context){
-    var curBook book
-    if err !=  c.BindJson(&curBook), err!=nil{
-      return
-    }
-    books = append(books, curBook)
-    c.IndentedJson(http.StatusCreated, curBook)
-    
+func postBook(c *gin.Context) {
+	var curBook book
+	if err := c.BindJson(&curBook); err != nil {
+		return
+	}
+	books = append(books, curBook)
+	c.IndentedJson(http.StatusCreated, curBook)
+
 }
 
 func main() {
-    router := gin.Default()
-    router.POST("/book")
-    router.Run("localhost:8080")
+	router := gin.Default()
+	router.POST("/book")
+	router.Run("localhost:8080")
 }
